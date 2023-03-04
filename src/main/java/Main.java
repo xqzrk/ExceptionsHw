05bb6df5d95adca4b97d36c240ee2ea2b7e424f6
MSevryukov <smlog@yandex.ru>
2023-03-04 11:47:20 +0300

    Homework version 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ProductRepository repo = new ProductRepository();
+        repo.removeById(15);
+    }
+}
---
+++ src/main/java/ProductRepository.java
@@ -0,0 +1,44 @@
+public class ProductRepository {
+
+    private Product[] products = new Product[0];
+
+    public void save(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        Product match = findById(id);
+        if (match == null) {
+            throw new NotFoundException(
+                    "Element with id: " + id + " not found"
+            );
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+    }
+}
